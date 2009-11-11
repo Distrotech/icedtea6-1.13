6616ada45bdacd5ab350f9fc17f89b8463d52d5e
Gary Benson <gbenson@redhat.com>
2009-11-11 10:30:19 +0000

    2009-11-11  Gary Benson  <gbenson@redhat.com>
    	* ports/hotspot/src/cpu/zero/vm/entry_zero.hpp
    	(ZeroEntry::entry_point): Inlined.
    	(ZeroEntry::invoke): Likewise.
    	(ZeroEntry::invoke_osr): Likewise.

--- ports/hotspot/src/cpu/zero/vm/entry_zero.hpp
+++ ports/hotspot/src/cpu/zero/vm/entry_zero.hpp
@@ -33,7 +33,7 @@ class ZeroEntry {
   address _entry_point;
 
  public:
-  address entry_point() const {
+  inline address entry_point() const {
     return _entry_point;
   }
   void set_entry_point(address entry_point) {
@@ -50,10 +50,10 @@ class ZeroEntry {
                                TRAPS);
 
  public:
-  void invoke(methodOop method, TRAPS) const {
+  inline void invoke(methodOop method, TRAPS) const {
     ((NormalEntryFunc) entry_point())(method, (intptr_t) this, THREAD);
   }
-  void invoke_osr(methodOop method, address osr_buf, TRAPS) const {
+  inline void invoke_osr(methodOop method, address osr_buf, TRAPS) const {
     ((OSREntryFunc) entry_point())(method, osr_buf, (intptr_t) this, THREAD);
   }
 
